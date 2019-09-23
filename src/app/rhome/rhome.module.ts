a3ad5a99529ca098dded8034846e9becafcad944
marlonlacerda2 <mdiogolacerda@gmail.com>
2019-09-23 00:02:56 -0300

    commit2

---
+++ src/app/rhome/rhome.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RhomePage } from './rhome.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RhomePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [RhomePage]
+})
+export class RhomePageModule {}
---
+++ src/app/rhome/rhome.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rhome',
+  templateUrl: './rhome.page.html',
+  styleUrls: ['./rhome.page.scss'],
+})
+export class RhomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
